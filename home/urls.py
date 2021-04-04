a453e47a8ba1931fe3655e44fda38d942c2829b5
avnexdigital <75736405+avnexdigital@users.noreply.github.com>
2021-04-04 19:34:23 +0530

    dlete

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('contact', views.contact, name='jbrg'),
-    path('search', views.search, name='search'),
-]
